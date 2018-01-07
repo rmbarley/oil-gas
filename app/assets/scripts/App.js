2a51b05ebe591641886224ec2189023464f6470e
rmbarley <rmbarley@gmail.com>
2018-01-07 17:50:25 -0500

    init

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,27 @@
+// Handle image toggle when user clicks on image map
+window.toggleView = function(view) {
+  if ($(view).hasClass('hide')) {
+    $('.mapster_tooltip').css('display', 'none')
+    $('.map_area').fadeToggle(300, 'linear', function() {
+      $('.map').trigger('click');
+      $(view).toggleClass('hide');
+    });
+  } else {
+    $(view).toggleClass('hide');
+    $('.map_area').fadeToggle(300, 'linear');
+  }
+}
+
+window.applyBlur = function() {
+  $('img[src$="_map.jpg"]').css({'filter': 'blur(3px)',
+    'transition': 'all 0.4s ease', '-ms-filter': 'blur(3px)'
+  });
+}
+window.removeBlur = function() {
+  $('img[src$="_map.jpg"]').css('filter', '');
+}
+
+// When modal closes, scroll to top of screen
+$(window).on('hidden.bs.modal', function (e) {
+  window.scrollTo(0,0);
+});
---
+++ app/temp/scripts/App.js
@@ -0,0 +1,100 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+// Handle image toggle when user clicks on image map
+window.toggleView = function(view) {
+  if ($(view).hasClass('hide')) {
+    $('.mapster_tooltip').css('display', 'none')
+    $('.map_area').fadeToggle(300, 'linear', function() {
+      $('.map').trigger('click');
+      $(view).toggleClass('hide');
+    });
+  } else {
+    $(view).toggleClass('hide');
+    $('.map_area').fadeToggle(300, 'linear');
+  }
+}
+
+window.applyBlur = function() {
+  $('img[src$="_map.jpg"]').css({'filter': 'blur(3px)',
+    'transition': 'all 0.4s ease', '-ms-filter': 'blur(3px)'
+  });
+}
+window.removeBlur = function() {
+  $('img[src$="_map.jpg"]').css('filter', '');
+}
+
+// When modal closes, scroll to top of screen
+$(window).on('hidden.bs.modal', function (e) {
+  window.scrollTo(0,0);
+});
+
+
+/***/ })
+/******/ ]);
